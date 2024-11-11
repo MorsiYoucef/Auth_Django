ba00e1ba6f44913f41c0b30f9adcf931d2d445af
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-11-11 21:08:09 +0100

    new commit

--- loginsignup/base/urls.py
+++ loginsignup/base/urls.py
@@ -1,10 +1,12 @@
 from .views import authView,home
+from django.contrib.auth import views as auth_views, logout
 from django.urls import path,include
 from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
      path("", home, name="home"),
+     path("logout/",logout, name="logout"),
      path("signup/", authView, name="authView"),
      path("accounts/", include("django.contrib.auth.urls")),
 
--- loginsignup/base/views.py
+++ loginsignup/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
 
@@ -10,15 +11,19 @@ def home(request):
     return render(request, "home.html", {})
 
 def authView(request):
+    form = UserCreationForm(request.POST or None)  # Initialize form for both GET and POST
+
     if request.method == 'POST':
-        form = UserCreationForm(request.POST or None)
         if form.is_valid():
             form.save()
-            return redirect("")
-    else:
-        form = UserCreationForm()
-        return render(request, "registration/signup.html", {"form": form})
+            return redirect("base:login")  # Redirect to login page after successful registration
 
+    # Render the form whether it’s a GET request or an invalid POST request
+    return render(request, "registration/signup.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
 # def authView(request):
 #     form = UserCreationForm()
 #     return render(request, "registration/signup.html", {"form": form})
